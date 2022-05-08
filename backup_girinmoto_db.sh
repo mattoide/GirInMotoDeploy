f8887a70d444d42370b942b39fda83a8f3c51fd8
matto <mattiabellotti.dev@gmail.com>
2022-05-08 20:00:48 +0200

    initial commit

---
+++ backup_girinmoto_db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ~/GirInMoto/cgsexpress_db_backups/
+
+NUMOFFILE=$(ls | wc -l)
+MAXACTIVEBCKP=20
+MAXDELETEFILE=10
+
+if [[ $NUMOFFILE -gt $MAXACTIVEBCKP ]]
+        then
+                echo  "Piu di $MAXACTIVEBCKP, elimino i primi $MAXDELETEFILE"
+                for FILE in $(ls -tr | grep "cgs*" | head -$MAXDELETEFILE)
+                        do
+                                rm  $FILE
+                        done
+else
+        echo "Sotto i $MAXACTIVEBCKP bkp"
+fi
+
+
+
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
+
+docker exec cgs_mysqldb_1 /usr/bin/mysqldump -u root --password=7DZeHNq19W6O express | gzip -9 > /root/CGS/cgsexpress_db_backups/cgsexpress_db_backup_$TIMESTAMP.gz
+
